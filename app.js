5dc46c4571d73b993fa7fa0c6211a2307f98e3a5
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2017-05-27 16:29:02 -0300

    Setting for routes

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
  * Created by ericpqmor on 27/05/17.
  */
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
 
-app.get("/", function (req, res) {
-   res.send("this will be the landing page");
-});
+//Set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/dashboard',express.static('./public'));
+
+//Loading all the routes
+// app.use(require('./routes'));
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by ericpqmor on 27/05/17.
  */
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
