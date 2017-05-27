3ad6456a73ae11f3b7ef1b9508dd0e27db7e175e
Eric Moreira <ericpqmor@gmail.com>
2017-05-27 16:05:57 -0300

    Adding initial job model

--- models/job.js
+++ models/job.js
@@ -1,3 +1,23 @@
 /**
  * Created by ericpqmor on 27/05/17.
  */
+
+var mongoose = require("mongoose");
+
+var jobSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    category: {
+        type: String,
+        enum : ['PONTUAL', 'CYCLIC'],
+        default: 'PONTUAL'
+    }/*,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }*/
+});
