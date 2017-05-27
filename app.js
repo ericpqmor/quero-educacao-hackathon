8b9010034edf5f1ff2ea30e672fb2561dfcb35a6
Eric Moreira <ericpqmor@gmail.com>
2017-05-27 15:45:57 -0300

    Adding npm monkey and initial routes

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 /**
  * Created by ericpqmor on 27/05/17.
  */
+
+var express = require('express');
+var app = express();
+
+app.get("/", function (req, res) {
+   res.send("this will be the landing page");
+});
+
+app.listen(3000, function (req, res) {
+    console.log("We are going to win this hackathon");
+});
+
+
